3c401846e274032d02541bccb283d158db9ae9c1
Gregor Billing <suushiemaniac@gmail.com>
2020-02-18 11:43:40 +0100

    Move symlink tracing responsibilities

--- buildSrc/src/main/kotlin/configurations/FileUtils.kt
+++ buildSrc/src/main/kotlin/configurations/FileUtils.kt
@@ -1,11 +1,15 @@
 package configurations
 
-import org.gradle.api.Project
+import org.gradle.api.Task
 import java.io.File
 import java.nio.file.Files
 
 object FileUtils {
-    fun Project.symlink(linkName: File, originFile: File) {
-        Files.createSymbolicLink(linkName.toPath(), originFile.toPath())
+    fun Task.symlink(linkName: File, originFile: File) {
+        if (!linkName.exists() || linkName.delete()) {
+            Files.createSymbolicLink(linkName.toPath(), originFile.toPath())
+        } else {
+            logger.warn("Unable to (re-)create symlink for $linkName!")
+        }
     }
 }
